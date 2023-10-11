6495117c22078b5f5b00146d7f4022a86e4873f0
Vladyslav Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-10-11 12:31:18 +0300

    Add Support Ukraine link

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navigation from "@/components/Navigation";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import UkraineFlagIcon from "@/components/UkraineFlagIcon";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,8 +42,15 @@ export default function RootLayout({
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
           <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
         ) : null}
-        <div className="bg-yellow-300 text-center text-black font-medium text-sm underline py-1">
-          🇺🇦 Support Ukraine
+        <div className="bg-gray-700 py-1">
+          <a href="https://war.ukraine.ua/support-ukraine/" target="_blank">
+            <div className="flex items-center justify-center gap-2">
+              <UkraineFlagIcon />
+              <span className="text-white text-md underline underline-offset-4">
+                Support Ukraine
+              </span>
+            </div>
+          </a>
         </div>
         <div className="mx-auto  max-w-2xl px-6">
           <Header />
---
+++ components/UkraineFlagIcon.tsx
@@ -0,0 +1,14 @@
+export default function UkraineFlagIcon() {
+  return (
+    <svg
+      width="32"
+      height="20"
+      viewBox="0 0 32 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect y="10" width="32" height="10" fill="#FFD500" />
+      <rect width="32" height="10" fill="#005BBB" />
+    </svg>
+  );
+}
